ed430d4e06570eee55382accbaa172326e937d73
Mindaugas Pikelis <mindaugas.pikelis@atea.lt>
2019-08-29 16:39:36 +0300

    Add project files.

---
+++ Delegates/Program.cs
@@ -0,0 +1,58 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    //delegate is:
+    //passing in a method instead of a parameter to a method.
+    class Program
+    {
+        static ShoppingCartModel cart = new ShoppingCartModel();
+        static void Main()
+        {
+            PopulateCartWithDemoData();
+
+            //passing in just the name of the method without any parentheses
+            Console.WriteLine($"The total for the cart is {cart.GenerateTotal(SubTotalAlert, CalculateLeveledDiscount):C2}");
+
+            Console.WriteLine();
+            Console.Write("Please press any key to exit the application...");
+            Console.ReadKey();
+        }
+
+        //declaring the delegate method
+        private static void SubTotalAlert(decimal subTotal)
+        {
+            Console.WriteLine($"The subtotal is {subTotal:C2}");
+        }
+
+        private static decimal CalculateLeveledDiscount(List<ProductModel> items, decimal subTotal)
+        {
+            if (subTotal > 100)
+            {
+                return subTotal * 0.80M;
+            }
+            else if (subTotal > 50)
+            {
+                return subTotal * 0.85M;
+            }
+            else if (subTotal > 10)
+            {
+                return subTotal * 0.95M;
+            }
+            else
+            {
+                return subTotal;
+            }
+        }
+
+        private static void PopulateCartWithDemoData()
+        {
+            cart.Items.Add(new ProductModel { ItemName = "Cereal", Price = 3.63M });
+            cart.Items.Add(new ProductModel { ItemName = "Milk", Price = 2.95M });
+            cart.Items.Add(new ProductModel { ItemName = "Strawberries", Price = 7.51M });
+            cart.Items.Add(new ProductModel { ItemName = "Blueberries", Price = 8.84M });
+        }
+    }
+}
---
+++ DemoLibrary/ShoppingCartModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoLibrary
+{
+    //its up to me if I want to use a delegate like "MentionDiscount" with the initialization
+    //or a Func, which desn't need an initialization (but it doesn't work with "out").
+    public class ShoppingCartModel
+    {
+        public delegate void MentionDiscount(decimal subTotal); //definition for a delegate <- kind of like an interface.
+        public List<ProductModel> Items { get; set; } = new List<ProductModel>();
+
+        //passing in the arbitrary delegate method
+        public decimal GenerateTotal(MentionDiscount mentionDiscount,
+            Func<List<ProductModel>, decimal, decimal> calculateDiscountetTotal)
+        {
+            decimal subTotal = Items.Sum(x => x.Price);
+
+            mentionDiscount(subTotal); // calling the method mentionDiscount and passing in the subTotal (loosly coupled, because genrateTotal doesnt know anything about mentionDiscount).
+
+            return calculateDiscountetTotal(Items, subTotal);
+        }
+    }
+}
